c803d2df3b0902cdce371f1c863d7b25955ce956
Ihor139 <igor.radchuk.13@gmail.com>
2021-10-10 16:29:27 +0300

    completed assembly Gulp 4

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,34 +1,14 @@
-console.log('Hi');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log(325235235);
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('asfasfasfasfasf');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('Hi43634634');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('Hihjghk657845784', 25235235235, 23523523.658, 56843);
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('H2i');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('H2O');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('K');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */ console.log('Si');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('Mg');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */console.log('Fe');
-/**
- * kjsdang;kjasn;sang'lksam ;
- */
+class ShowLog {
+  constructor(message, age) {
+    this.message = message
+    this.age = age
+  }
+  sayHi() {
+    console.log(this.message, this.age)
+  }
+}
+
+
+const showLog = new ShowLog("Привет, мне", 24)
+
+showLog.sayHi()
